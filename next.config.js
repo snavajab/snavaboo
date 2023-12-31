aa91901b80b14180f55331f7ac72574736e1251e
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-12-31 18:33:20 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
+let assetPrefix = ""
+let basePath = ""
+
+if (process.env.GITHUB_ACTIONS) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+  // check if the repo name doesn't end with .github.io
+  if (!/\.github\.io$/.test(repo)) {
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+  }
+}
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
