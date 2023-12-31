1f51ad2ea4bba7b43be2a4232deb25c2673e73bb
Tommaso Morganti <tommaso.morganti01@gmail.com>
2023-12-31 18:40:34 +0100

    hardcode

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-let assetPrefix = ""
-let basePath = ""
-
-if (process.env.GITHUB_ACTIONS) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-  // check if the repo name doesn't end with .github.io
-  if (!/\.github\.io$/.test(repo)) {
-    assetPrefix = `/${repo}/`
-    basePath = `/${repo}`
-  }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  assetPrefix: "/snavaboo/",
+  basePath: "/snavaboo",
   images: {
     unoptimized: true,
   },
